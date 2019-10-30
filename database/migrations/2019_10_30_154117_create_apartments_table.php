aad59f6831243918b7a26acf3788748d62a1e4a5
Edoardo <a@Air-di-edo.lan>
2019-10-30 17:47:35 +0100

    agg. appartamenti

---
+++ database/migrations/2019_10_30_154117_create_apartments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('description')->nullable();
+            $table->integer('rooms')->nullable();
+            $table->integer('beds')->nullable();
+            $table->integer('bathrooms')->nullable();
+            $table->integer('mq')->nullable();
+            $table->string('geo_coords')->nullable();
+            $table->integer('visualizations')->nullable();
+            $table->boolean('active')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
