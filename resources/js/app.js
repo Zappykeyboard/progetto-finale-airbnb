54118fc16aa2e6f826e23b0f99403f290bf11473
Zappykeyboard <Zappykeyboard@users.noreply.github.com>
2019-11-16 20:12:12 +0100

    fix apt edit

--- resources/js/app.js
+++ resources/js/app.js
@@ -42,16 +42,13 @@ function init(){
   var token = $('meta[name="csrf-token"]').attr('content');
   window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
 
-  console.log(token);
 
   var create_apt = new Vue({
       el: '#app'
   });
 
   // filePondDropImg();
-  var App = new Vue({
-    el:'#app'
-  });
+
 
 
   var message_comp = new Vue({
